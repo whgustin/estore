cc1e6243a89c0fb783e0135d5fc8c4347bf18bbc
Wesley Gustin <wes-gustin@Wesleys-MBP.attlocal.net>
2020-09-06 16:23:02 -0500

    initial menu works, ready for transitions

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 function Header() {
     return (
-        <header className="border-b font-bold p-3">
+        <header className="border-b p-3 flex justify-between items-center">
+            <span className="font-bold">
             App Name
+            </span>
+
+            <Navigation />
         </header>
     )
 }
 
-export default Header
+export default Header 
