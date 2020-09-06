89237698fab59d29778334bb72b93b7df904ec5b
Wesley Gustin <wes-gustin@Wesleys-MBP.attlocal.net>
2020-09-06 15:37:01 -0500

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className="border-b font-bold p-3">
+            App Name
+        </header>
+    )
+}
+
+export default Header
